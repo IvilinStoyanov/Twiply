8afcaced19854bb4f52f4314345281d3212f7dff
IvilinStoyanov <ivilin.stoyanov9@gmail.com>
2019-04-29 20:53:51 +0300

    add home and register components

---
+++ src/DateApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancel');
+  }
+
+}
