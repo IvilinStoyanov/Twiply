c15c319e85e610bf3f14adc82cd2c1f737372019
IvilinStoyanov <ivilin.stoyanov9@gmail.com>
2019-06-30 20:44:30 +0300

    add multiple route protecting

--- src/DateApp-SPA/src/app/routes.ts
+++ src/DateApp-SPA/src/app/routes.ts
@@ -6,9 +6,16 @@ import { ListComponent } from './list/list.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-    { path: 'messages', component: MessagesComponent},
-    { path: 'lists', component: ListComponent},
-    { path: '**', redirectTo: 'home', pathMatch: 'full'}
+    { path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListComponent}
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
