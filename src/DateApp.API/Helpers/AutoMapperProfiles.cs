cdb113196ea90eadae2b1470dd72ef5f908fcb71
IvilinStoyanov <ivilin.stoyanov9@gmail.com>
2019-11-02 21:58:29 +0200

    add Photo upload functionality

--- src/DateApp.API/Helpers/AutoMapperProfiles.cs
+++ src/DateApp.API/Helpers/AutoMapperProfiles.cs
@@ -3,21 +3,26 @@ using AutoMapper;
 using DateApp.API.Dtos;
 using DateApp.API.Models;
 
-namespace DateApp.API.Helpers {
-    public class AutoMapperProfiles : Profile {
-        public AutoMapperProfiles () {
-            CreateMap<User, UserForListDto> ()
-                .ForMember (dest => dest.PhotoUrl,
-                    opt => opt.MapFrom (src => src.Photos.FirstOrDefault (p => p.IsMain).Url))
-                .ForMember (dest => dest.Age,
-                    opt => opt.MapFrom (src => src.DateOfBirth.CalculateAge ()));
-            CreateMap<User, UserForDetailedDto> ()
-                .ForMember (dest => dest.PhotoUrl,
-                    opt => opt.MapFrom (src => src.Photos.FirstOrDefault (p => p.IsMain).Url))
-                .ForMember (dest => dest.Age,
-                    opt => opt.MapFrom (src => src.DateOfBirth.CalculateAge ()));
-            CreateMap<Photo, PhotoForDetailedDto> ();
+namespace DateApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl,
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl,
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }
     }
 }
