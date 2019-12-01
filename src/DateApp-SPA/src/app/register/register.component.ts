ad3859e7917f70521b06270cf75571bce3e5efed
IvilinStoyanov <ivilin.stoyanov9@gmail.com>
2019-12-02 00:59:42 +0200

    add reactive forms in register component

--- src/DateApp-SPA/src/app/register/register.component.ts
+++ src/DateApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,20 +8,27 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  model: any = {};
   @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+  registerForm: FormGroup;
 
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
   }
 
   register() {
-   this.authService.register(this.model).subscribe(() => {
-     console.log('registration ok');
-   }, error => {
-     console.log(error);
-   });
+  //  this.authService.register(this.model).subscribe(() => {
+  //    console.log('registration ok');
+  //  }, error => {
+  //    console.log(error);
+  //  });
+  console.log(this.registerForm.value);
   }
 
   cancel() {
