f1a3e3ac6905b5dff5248dbf25ddb6a682b18a5e
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2020-01-24 18:41:36 +0200

    add messages functionality from API

---
+++ src/DateApp.API/Controllers/MessagesController.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using DateApp.API.Data.Repository.Contracts;
+using DateApp.API.Dtos;
+using DateApp.API.Helpers;
+using DateApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DateApp.API.Controllers
+{
+    [ServiceFilter(typeof(LogUserActivity))]
+    [Authorize]
+    [Route("api/users/{userId}/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+        public MessagesController(IDatingRepository repo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repo = repo;
+        }
+
+        [HttpGet("{id}", Name = "GetMessage")]
+        public async Task<IActionResult> GetMessage(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messageFromRepo = await _repo.GetMessage(id);
+
+            if (messageFromRepo == null)
+                return NotFound();
+
+            return Ok(messageFromRepo);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMessagesForUser(int userId, [FromQuery]MessageParams messageParams)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            messageParams.UserId = userId;
+
+            var messagesFromRepo = await _repo.GetMessagesForUser(messageParams);
+
+            var messages = _mapper.Map<IEnumerable<MessageToRuturnDto>>(messagesFromRepo);
+
+            Response.AddPagination(messagesFromRepo.CurrentPage, messagesFromRepo.PageSize,
+             messagesFromRepo.TotalCount, messagesFromRepo.TotalPages);
+
+            return Ok(messages);
+        }
+
+        [HttpGet("thread/{recipientId}")]
+        public async Task<IActionResult> GetMessageThread(int userId, int recipientId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messagesFromRepo = await _repo.GetMessagesThread(userId, recipientId);
+
+            var messageThread = _mapper.Map<IEnumerable<MessageToRuturnDto>>(messagesFromRepo);
+
+            return Ok(messageThread);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            messageForCreationDto.SenderId = userId;
+
+            var recipient = await _repo.GetUser(messageForCreationDto.RecipientId);
+
+            if (recipient == null)
+                return BadRequest("Could not find user");
+
+            var message = _mapper.Map<Message>(messageForCreationDto);
+
+            _repo.Add(message);
+
+            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
+
+            if (await _repo.SaveAll())
+                return CreatedAtRoute("GetMessage", new { id = message.Id }, messageToReturn);
+
+            throw new Exception("Creation the message failed on save");
+        }
+    }
+}
--- src/DateApp.API/Data/Repository/Contracts/IDatingRepository.cs
+++ src/DateApp.API/Data/Repository/Contracts/IDatingRepository.cs
@@ -15,5 +15,8 @@ namespace DateApp.API.Data.Repository.Contracts
         Task<Photo> GetPhoto(int id);
         Task<Photo> GetMainPhotoForUser(int userId);
         Task<Like> GetLike(int userId, int recipientId);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams);
+        Task<IEnumerable<Message>> GetMessagesThread(int userId, int recipientId);
     }
 }
