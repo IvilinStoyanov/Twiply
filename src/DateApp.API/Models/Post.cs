2fd10213bc15b1aff344ab61ab43893b259fa952
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2020-03-25 21:46:18 +0200

    add ability user to make posts

--- src/DateApp.API/Models/Post.cs
+++ src/DateApp.API/Models/Post.cs
@@ -4,6 +4,10 @@ namespace DateApp.API.Models
 {
     public class Post
     {
+        public Post()
+        {
+            Created = DateTime.Now;
+        }
         public int Id { get; set; }
         public string Description { get; set; }
         public DateTime Created { get; set; }
