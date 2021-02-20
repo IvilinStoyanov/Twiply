09ab83dc39dbe116a0fbe38ef0213031c622401f
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2021-02-21 00:04:02 +0200

    adding initial database migration

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,21 +18,20 @@ namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => 
+            services.AddDbContext<DataContext>(options =>
             {
-                options.UseSqlServer("Connection string");
+                options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
-            
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
