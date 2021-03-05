0d7ab7e601328edc4c0a1199431fc3ab18fb3d18
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2021-03-05 23:35:27 +0200

    adding seed functionality, adding user repository

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -22,7 +23,6 @@ namespace API.Controllers
            return await _context.Users.ToListAsync();
         }
 
-        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id) 
         {
