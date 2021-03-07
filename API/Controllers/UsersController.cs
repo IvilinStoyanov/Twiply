50e6b83340052afc243871798743538d9af3f9b5
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2021-03-07 11:40:42 +0200

    adding automapper queryable extensions

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,10 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using API.Data;
 using API.DTOs;
-using API.Entities;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -25,19 +22,13 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await _userRepository.GetUsersAsync();
-            
-            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
-            
-            return Ok(usersToReturn);
+            return Ok(await _userRepository.GetMembersAsync());
         }
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            var user =  await _userRepository.GetUserByUsernameAsync(username);
-
-            return _mapper.Map<MemberDto>(user);
+            return (await _userRepository.GetMemberAsync(username));
         }
     }
 }
