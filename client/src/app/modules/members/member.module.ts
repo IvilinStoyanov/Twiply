567ebb8aa13bd32eaa8ce1ef9c2b8ddc4d53158c
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2021-03-08 10:23:33 +0200

    adding basic html structure of the member list

--- client/src/app/modules/members/member.module.ts
+++ client/src/app/modules/members/member.module.ts
@@ -4,11 +4,13 @@ import { MemberComponent } from './member.component';
 import { MemberRoutingModule } from './member-routing.module';
 import { MemberDetailComponent } from './components/member-detail/member-detail.component';
 import { MemberListComponent } from './components/member-list/member-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MemberRoutingModule
+    MemberRoutingModule,
+    SharedModule
   ],
   declarations: [MemberComponent, MemberDetailComponent, MemberListComponent]
 })
