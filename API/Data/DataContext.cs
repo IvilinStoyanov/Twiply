49d0d74052eb184367b4fdc60e65984d9f63f7cb
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2021-05-29 17:02:00 +0300

    message entity added + migration

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,5 +11,21 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesRecieve)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
