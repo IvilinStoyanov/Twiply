5cbb9f4f4613639301f85a9877de738ce5360c8c
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-01-28 20:56:24 +0200

    add chats methods and basic setup design

--- client/src/app/modules/messages/components/message-container/message-container.component.ts
+++ client/src/app/modules/messages/components/message-container/message-container.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Pagination } from 'src/app/models/pagination';
+import { Member } from 'src/app/models/member';
+import { Message } from 'src/app/models/message';
+import { PaginatedResult, Pagination } from 'src/app/models/pagination';
+import { MembersService } from 'src/app/modules/members/services/members.service';
 import { MessageService } from '../../services/message.service';
 
 @Component({
@@ -8,16 +11,28 @@ import { MessageService } from '../../services/message.service';
   styleUrls: ['./message-container.component.scss']
 })
 export class MessageContainerComponent implements OnInit {
-  messages: any = [];
+  members: Member[];
+  messages: Message[] = [];
   pagination: Pagination;
   pageNumber = 1;
-  pageSize = 5;
+  pageSize = 100;
   loading = false;
 
-  constructor(private messageService: MessageService) { }
+  constructor(private membersService: MembersService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.loadLikedUsers();
     this.loadMessages();
+  }
+
+  // TODO: Change later to the filtered method
+  loadLikedUsers() {
+      this.membersService
+        .getMembers(this.pageNumber, this.pageSize)
+        .subscribe((response) => {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        });
   }
 
   loadMessages(type: string = 'Unread') {
