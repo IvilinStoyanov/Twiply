f64e6d401a4edeb4c114bf29ef05959e02d56578
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-07-15 13:11:35 +0300

    add production build in the wwwroot folder

--- client/src/app/interceptors/loading.interceptor.ts
+++ client/src/app/interceptors/loading.interceptor.ts
@@ -17,7 +17,6 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.loaderService.busy();
     return next.handle(request).pipe(
-      delay(1000),
       finalize(() => {
         this.loaderService.idle();
       })
